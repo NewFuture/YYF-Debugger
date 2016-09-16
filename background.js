b96568f8afa530159d7bc47bc16cab3eeee5c204
NewFuture <new_future@msn.cn>
2016-09-16 21:42:02 +0800

    :confounded: show tracer and log and color

--- background.js
+++ background.js
@@ -1,171 +1,171 @@
 /* globals alert, chrome */
 (function() {
-    'use strict';
+  'use strict';
 
-    var active = false;
-    var ACTIVE_ICON = '';
-    var ACTIVE_NAME = 'Debugging YYF';
-    var INACTIVE_ICON = '';
-    var INACTIVE_NAME = 'YYF Debugger';
+  var active = false;
+  var ACTIVE_ICON = '';
+  var ACTIVE_NAME = 'YYF (Debugging)';
+  var INACTIVE_ICON = '';
+  var INACTIVE_NAME = 'YYF Debugger (stop)';
 
-    // list of all tabs with chrome logger enabled
-    var tabsWithExtensionEnabled = [];
+  // list of all tabs with chrome logger enabled
+  var tabsWithExtensionEnabled = [];
 
-    /**
-     * handles a click on the extension icon
-     */
-    function _handleIconClick(tab) {
-        if (tab) {
-            _toggleActivity(tab);
-        }
+  /**
+   * handles a click on the extension icon
+   */
+  function _handleIconClick(tab) {
+    if (tab) {
+      _toggleActivity(tab);
+    }
+  }
+
+  function _toggleActivity(tab) {
+    var url = tab.url;
+    var host = _getHost(url);
+    if (_hostIsActive(host)) {
+      delete localStorage[host];
+      _deactivate(tab.id);
+      return;
+    }
+    localStorage[host] = true;
+    _activate(tab.id);
+  }
+
+  function _getHost(url) {
+    url = url.replace(/^(https?:\/\/)/, '', url);
+    var host = url.split('/')[0];
+    return host;
+  }
+
+  function _hostIsActive(url) {
+    return localStorage[url] === "true";
+  }
+
+  function _activate(tabId) {
+    active = true;
+    if (tabsWithExtensionEnabled.indexOf(tabId) === -1) {
+      tabsWithExtensionEnabled.push(tabId);
+    }
+    _setTitle(tabId, ACTIVE_NAME);
+  }
+
+  function _deactivate(tabId) {
+    active = false;
+    var index = tabsWithExtensionEnabled.indexOf(tabId);
+    if (index !== -1) {
+      tabsWithExtensionEnabled.splice(index, 1);
+    }
+    _setTitle(tabId, INACTIVE_NAME);
+  }
+
+  function _setIcon(iconPath) {
+    chrome.browserAction.setIcon({
+      path: iconPath
+    });
+  }
+
+  function _setTitle(tabId, titleName) {
+    chrome.browserAction.getTitle({
+      tabId: tabId
+    }, function(title) {
+      chrome.browserAction.setTitle({
+        title: titleName,
+        tabId: tabId
+      });
+    });
+  }
+
+  /**
+   * A tab has become active.
+   * https://developer.chrome.com/extensions/tabs#event-onActivated
+   *
+   * @param   {[type]}  activeInfo
+   *
+   * @return  void
+   */
+  function _handleTabActivated(activeInfo) {
+    // This is sometimes undefined but an integer is required for chrome.tabs.get
+    if (typeof activeInfo.tabId != 'number') {
+      return;
+    }
+    chrome.tabs.get(activeInfo.tabId, _handleTabEvent);
+  }
+
+  /**
+   * A tab was updated.
+   * https://developer.chrome.com/extensions/tabs#event-onUpdated
+   *
+   * @param   integer  tabId
+   * @param   object   changeInfo
+   * @param   object   tab
+   *
+   * @return  void
+   */
+  function _handleTabUpdated(tabId, changeInfo, tab) {
+    _handleTabEvent(tab);
+  }
+
+  /**
+   * Handle an event for any tab. Activate or deactivate the extension for the current tab.
+   *
+   * @param   object  tab
+   *
+   * @return  void
+   */
+  function _handleTabEvent(tab) {
+    if (!(tab && tab.active)) {
+      return;
+    }
+    var id = (typeof tab.id === 'number') ? tab.id : tab.sessionID;
+
+    if (typeof id === 'undefined') {
+      return;
+    }
+    if (_hostIsActive(_getHost(tab.url))) {
+      _activate(id);
+      return;
     }
 
-    function _toggleActivity(tab) {
-        var url = tab.url;
-        var host = _getHost(url);
-        if (_hostIsActive(host)) {
-            delete localStorage[host];
-            _deactivate(tab.id);
-            return;
-        }
-        localStorage[host] = true;
-        _activate(tab.id);
-    }
+    _deactivate(id);
+  }
 
-    function _getHost(url) {
-        url = url.replace(/^(https?:\/\/)/, '', url);
-        var host = url.split('/')[0];
-        return host;
-    }
+  function _addListeners() {
+    var queuedRequests = [];
+    chrome.browserAction.onClicked.addListener(_handleIconClick);
+    chrome.tabs.onActivated.addListener(_handleTabActivated);
+    chrome.tabs.onCreated.addListener(_handleTabEvent);
+    chrome.tabs.onUpdated.addListener(_handleTabUpdated);
 
-    function _hostIsActive(url) {
-        return localStorage[url] === "true";
-    }
-
-    function _activate(tabId) {
-        active = true;
-        if (tabsWithExtensionEnabled.indexOf(tabId) === -1) {
-            tabsWithExtensionEnabled.push(tabId);
-        }
-        _setTitle(tabId, ACTIVE_NAME);
-    }
-
-    function _deactivate(tabId) {
-        active = false;
-        var index = tabsWithExtensionEnabled.indexOf(tabId);
-        if (index !== -1) {
-            tabsWithExtensionEnabled.splice(index, 1);
-        }
-        _setTitle(tabId, INACTIVE_NAME);
-    }
-
-    function _setIcon(iconPath) {
-        chrome.browserAction.setIcon({
-            path: iconPath
+    chrome.webRequest.onResponseStarted.addListener(function(details) {
+      if (tabsWithExtensionEnabled.indexOf(details.tabId) !== -1) {
+        chrome.tabs.sendMessage(details.tabId, {
+          name: "header_update",
+          details: details
+        }, function(response) {
+          if (!response) {
+            queuedRequests.push(details);
+          }
         });
-    }
+      }
+    }, {
+      urls: ["<all_urls>"]
+    }, ["responseHeaders"]);
 
-    function _setTitle(tabId, titleName) {
-        chrome.browserAction.getTitle({
-            tabId: tabId
-        }, function(title) {
-            chrome.browserAction.setTitle({
-                title: titleName,
-                tabId: tabId
-            });
-        });
-    }
-
-    /**
-     * A tab has become active.
-     * https://developer.chrome.com/extensions/tabs#event-onActivated
-     *
-     * @param   {[type]}  activeInfo
-     *
-     * @return  void
-     */
-    function _handleTabActivated(activeInfo) {
-        // This is sometimes undefined but an integer is required for chrome.tabs.get
-        if (typeof activeInfo.tabId != 'number') {
-            return;
-        }
-        chrome.tabs.get(activeInfo.tabId, _handleTabEvent);
-    }
-
-    /**
-     * A tab was updated.
-     * https://developer.chrome.com/extensions/tabs#event-onUpdated
-     *
-     * @param   integer  tabId
-     * @param   object   changeInfo
-     * @param   object   tab
-     *
-     * @return  void
-     */
-    function _handleTabUpdated(tabId, changeInfo, tab) {
-        _handleTabEvent(tab);
-    }
-
-    /**
-     * Handle an event for any tab. Activate or deactivate the extension for the current tab.
-     *
-     * @param   object  tab
-     *
-     * @return  void
-     */
-    function _handleTabEvent(tab) {
-        if (!(tab && tab.active)) {
-            return;
-        }
-        var id = (typeof tab.id === 'number') ? tab.id : tab.sessionID;
-
-        if (typeof id === 'undefined') {
-            return;
-        }
-        if (_hostIsActive(_getHost(tab.url))) {
-            _activate(id);
-            return;
-        }
-
-        _deactivate(id);
-    }
-
-    function _addListeners() {
-        var queuedRequests = [];
-        chrome.browserAction.onClicked.addListener(_handleIconClick);
-        chrome.tabs.onActivated.addListener(_handleTabActivated);
-        chrome.tabs.onCreated.addListener(_handleTabEvent);
-        chrome.tabs.onUpdated.addListener(_handleTabUpdated);
-
-        chrome.webRequest.onResponseStarted.addListener(function(details) {
-            if (tabsWithExtensionEnabled.indexOf(details.tabId) !== -1) {
-                chrome.tabs.sendMessage(details.tabId, {
-                    name: "header_update",
-                    details: details
-                }, function(response) {
-                    if (!response) {
-                        queuedRequests.push(details);
-                    }
-                });
-            }
-        }, {
-            urls: ["<all_urls>"]
-        }, ["responseHeaders"]);
-
-        chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-            switch (request) {
-                case 'localStorage':
-                    sendResponse(localStorage);
-                    break;
-                case 'isActive':
-                    sendResponse(active);
-                    break;
-                case 'ready':
-                    sendResponse(queuedRequests);
-                    queuedRequests = [];
-                    break;
-            }
-        });
-    }
-    _addListeners();
+    chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+      switch (request) {
+        case 'localStorage':
+          sendResponse(localStorage);
+          break;
+        case 'isActive':
+          sendResponse(active);
+          break;
+        case 'ready':
+          sendResponse(queuedRequests);
+          queuedRequests = [];
+          break;
+      }
+    });
+  }
+  _addListeners();
 })();
--- main.js
+++ main.js
@@ -1,100 +1,367 @@
 /* globals chrome, console */
 (function() {
-    'use strict';
-
-    var PRE_HEADER = 'Yyf-Debug-';
-    var local_storage = null;
-
-
-    function _process(detail) {
-        console.group('%s %s [%i]', detail.method, detail.url, detail.statusCode);
-        // console.log(detail);
-        detail.responseHeaders.forEach(_displayHeader);
-        console.groupEnd();
+  'use strict';
+  var local_storage = null;
+  var YYF_FLAG = 'YYF';
+  var PRE_HEADER = 'yyf-';
+  var TRACER_KEY = ['time', 'mem', 'file'];
+  /**不同数据显示样式*/
+  var CSS = {
+    DEFAULT: ';',
+    URL: 'color:black;font-style: italic;',
+    YYF: 'color:teal;',
+    SERVER: 'color:yellowgreen;',
+    FILE: 'color:DimGrey;',
+    GRAY: 'color:Gray;',
+    SQL: 'color:OliveDrab;font-weight:lighter;',
+    ERROR: 'color:red;font-weight:bolder',
+    DUMP: 'color:DodgerBlue;font-weight:bolder;',
+    TRACER: 'color:MediumSlateBlue;font-weight:lighter;',
+    METHOD: function(method) { //请求方式样式
+      var color = 'Black '
+      switch (method) {
+        case 'GET':
+          color = 'DarkGreen';
+          break;
+        case 'POST':
+          color = 'DarkBlue';
+          break;
+        case 'PUT':
+          color = 'DarkOrange'; //'DarkMagenta';
+          break;
+        case 'DELETE':
+          color = 'DarkViolet'; //'Brown';
+          break;
+      }
+      return 'color:' + color + ';font-weight: bolder;';
+    },
+    STATUS: function(code) { //请求状态码样式
+      var color =
+        code < 200 ? 'black' :
+        code < 300 ? 'LimeGreen' :
+        code < 400 ? 'orange' :
+        code < 500 ? 'yellow' :
+        'red';
+      return 'color:' + color + '; font-size:small;font-weight:lighter;';
     }
+  };
 
-    //解析显示header
-    function _displayHeader(header) {
-        if (header.name.indexOf(PRE_HEADER) === 0) {
-            var headername = header.name.split('-');
-            var len = headername.length;
-            if (len > 1) {
-                var value = header.value;
-                switch (headername[len - 1]) {
-                    case 'O':
-                        value = JSON.parse(value);
-                        break;
-                    case 'S':
-                    case 'N':
-                        value = decodeURI(value);
-                        break;
-                    case 'J':
-                        value = JSON.parse(value);
-                        break;
-                    case 'B':
-                        value = (value === 'true');
-                        break;
-                }
-                var type = header.name.slice(PRE_HEADER.length, -2);
-                switch (type.toLowerCase()) {
-                    case 'info':
-                        console.info(value);
-                        break;
-                    case 'warn':
-                        console.warn(value);
-                        break;
-                    case 'error':
-                        console.error(value);
-                        break;
-                    case 'log':
-                        console.log(value);
-                        break;
-                    default:
-                        console.debug("%s:%s", type, value);
-                }
 
-            }
+
+  function _process(detail) {
+    var version = detail.responseHeaders.find(function(h) {
+      return h.name === YYF_FLAG;
+    });
+    if (version) {
+      var server = detail.responseHeaders.find(function(h) {
+        return h.name === "X-Powered-By";
+      });
+      server = undefined === server ? '' : ' ' + server.value;
+      console.group(
+        '%c' + detail.method + ' %c' + detail.url + ' %c[%c' + detail.statusCode + '%c](YYF:%c' + version.value + '%c' + server + '%c)',
+        CSS.METHOD(detail.method),
+        CSS.STATUS(detail.statusCode),
+        CSS.DEFAULT,
+        CSS.STATUS(detail.statusCode),
+        CSS.DEFAULT,
+        CSS.YYF,
+        CSS.SERVER,
+        CSS.DEFAULT
+      );
+      detail.responseHeaders.forEach(_displayHeader);
+      console.groupEnd();
+    }
+  }
+
+  //解析显示header
+  function _displayHeader(header) {
+    var key = header.name.toLowerCase();
+    if (key.startsWith(PRE_HEADER.toLowerCase())) {
+      key = key.substr(PRE_HEADER.length).toLowerCase();
+      var value = header.value;
+
+      if (key.includes('-')) {
+        var name = key.split('-');
+        if (name[0] === 'sql') {
+          _showSQL(value, name[1]);
+        } else {
+          //dump messge
+          _showDump(name.shift(), name.join('-'), value);
         }
-    }
-
-    function _handleHeaderUpdate(request, sender, sendResponse) {
-        if (request.name === "header_update") {
-            // if this is not a header update don't do anything
-            _process(request.details);
-            return sendResponse("done");
+      } else {
+        if (TRACER_KEY.includes(key)) {
+          _showTrace(key, value);
+        } else {
+          _showLog(key, value);
         }
+      }
+    }
+  }
+
+  //dump 变量
+  function _showDump(type, name, value) {
+    switch (type) {
+      case 'e':
+        value = decodeURI(value);
+      case 's':
+        type = 'string';
+        break;
+
+      case 'n':
+        type = 'number';
+        value = Number(value);
+        break;
+
+      case 'b':
+        type = 'boolean';
+        value = ('true' == value) || (1 == value);
+        break;
+
+      case 'o': //object
+      case 'j': //json
+        value = JSON.parse(value);
+        type = Array.isArray(value) ? 'array' : typeof(value);
+        if ('object' == type) { //display object
+          // formater = '%c[%s (%s)]: %c%o';
+          type = value.__CLASS__ || 'associative array'; //php 关联数组
+        }
+        break;
+
+      case 'u': //unkown
+        value = decodeURI(value);
+      default:
+        type = 'unkown(' + type + ')';
+    }
+    console.log('%c[%c%s%c (%s)]:', CSS.GRAY, CSS.DUMP, name, CSS.GRAY, type, value);
+  }
+
+  //sql 查询结果
+  function _showSQL(data, id) {
+    id = "00".substring(0, 2 - id.length) + id
+    data = JSON.parse(data);
+    console.groupCollapsed("%c[SQL %s] %cSQL查询统计 (耗时: %f ms) %c[%s...]",
+      CSS.SQL,
+      id,
+      data.E ? CSS.ERROR : CSS.DEFAULT,
+      data.T,
+      CSS.GRAY,
+      data.Q.substring(0, 10)
+    );
+    var output = {
+      '耗时(ms)': {
+        'value': data.T
+      },
+      '查询语句': {
+        'value': data.Q
+      },
+    };
+
+    //错误状态
+    if (data.E) {
+      output['查询出错'] = {
+        'value': JSON.stringify(data.E)
+      };
+      //依次解析各个查询参数
+      output["SQLSTATE"] = {
+        'value': data.E[0]
+      };
+      output["驱动错误码"] = {
+        'value': data.E[1]
+      };
+      output["驱动错误信息"] = {
+        'value': data.E[2]
+      };
+      console.error('SQL 查询出错[%s]：%s (%i)', data.E[0], data.E[2], data.E[1]);
+    }
+    //参数
+    if (data.P) {
+      output['查询参数'] = {
+          'value': JSON.stringify(data.P)
+        }
+        //依次解析各个查询参数
+      for (var p in data.P) {
+        output["参数" + p] = {
+          'value': data.P[p]
+        };
+      }
+    }
+    //结果
+    if (data.R instanceof Object) {
+      output['查询结果'] = {
+        'value': JSON.stringify(data.R)
+      };
+      //依次解析各个查询结果
+      for (var i in data.R) {
+        var r = data.R[i];
+        output['结果:' + i] = {
+          'value': (r instanceof Object) ? JSON.stringify(r) : r
+        };
+      }
+    } else {
+      output['查询结果'] = {
+        'value': data.R
+      };
+    }
+    console.table(output);
+    if (Number.isInteger && Number.isInteger(data.R)) {
+      console.info('tips: 为了数据安全,默认只输出[查询结果]条数,可以修改服务器上[conf/app.ini]配置debug.sql.result=1以显示完整结果');
+    }
+    console.groupEnd();
+  }
+
+  /**日志输出
+   * 状态 emergency,alert,critical,error,warn,notice,info,debug,
+   */
+  function _showLog(type, data) {
+    var logmsg = '%c[' + type + ']';
+    switch (type) {
+      case 'emergency':
+      case 'alert':
+        alert('[' + type + ']' + data);
+      case 'critical':
+      case 'error':
+        console.error(logmsg, CSS.GRAY, data);
+        break;
+
+      case 'warning':
+      case 'warn':
+      case 'notice':
+        console.warn(logmsg, CSS.GRAY, data);
+        break;
+
+      case 'info':
+        console.info(logmsg, CSS.GRAY, data);
+        break;
+
+      case 'debug':
+        console.debug(logmsg, CSS.GRAY, data);
+        break;
+      case 'assert': //断言
+        console.assert(false, data);
+        break;
+      default:
+        console.log(logmsg, CSS.GRAY, data);
+    }
+  }
+
+  //系统资源消耗信息信息
+  function _showTrace(type, data) {
+    try {
+      data = JSON.parse(data);
+    } catch (e) {
+      return _showLog(type, data);
     }
 
-    function _listenForLogMessages() {
-        chrome.extension.onMessage.addListener(_handleHeaderUpdate);
-    }
+    switch (type) {
+      case 'mem': //内存
+        console.groupCollapsed('%c[memory]%c 内存消耗统计 (峰值: %i KB)',
+          CSS.TRACER,
+          CSS.DEFAULT,
+          data.M);
+        data = {
+          '框架启动': {
+            'consume[KB]': data.S,
+            'comment': '加载框架所消耗内存'
+          },
+          '程序运行': {
+            'consume[KB]': data.U,
+            'comment': '程序运行额外使用内存'
+          },
+          '内存峰值': {
+            'consume[KB]': data.M,
+            'comment': '整个请求内存占用峰值'
+          }
+        }
+        console.table(data);
+        console.info('tips: 开发环境加载调试插件会消耗额外内存');
+        break;
 
-    function _stopListening() {
-        chrome.extension.onMessage.removeListener(_handleHeaderUpdate);
-    }
+      case 'time': //时间
+        var total = data.S + data.P + data.U;
+        total = Math.round(total * 1000) / 1000;
+        console.groupCollapsed('%c[_time_]%c 时间消耗统计 (总计: %f ms)',
+          CSS.TRACER,
+          CSS.DEFAULT,
+          total);
+        data = {
+          '框架启动': {
+            'consume[ms]': data.S,
+            'comment': '从获取请求到框架启动完成'
+          },
+          '分发处理': {
+            'consume[ms]': data.P,
+            'comment': 'Bootstrap加载插件和分发'
+          },
+          '程序运行': {
+            'consume[ms]': data.U,
+            'comment': '业务程序处理和输出耗时'
+          },
+          '耗时总计': {
+            'consume[ms]': total,
+            'comment': '从请求到处理完成结束耗时'
+          }
+        }
+        console.table(data);
+        console.info('tips: 启动阶段:包括服务器url重写耗时(这部分不属于PHP)');
+        console.info('tips: 分发阶段：开发环境加载调试插件会占用大部分时间');
+        break;
 
-    function _initStorage() {
-        chrome.extension.sendMessage("localStorage", function(response) {
-            local_storage = response;
+      case 'file': //文件
+        console.groupCollapsed('%c[_file_]%c 文件加载统计 (总计: %i)',
+          CSS.TRACER,
+          CSS.DEFAULT,
+          data.length
+        );
+        data.forEach(function(file, i) {
+          console.log('[%i] %c%s', i, CSS.FILE, file);
         });
-    }
+        console.info('tips: 调试相关文件(library/Debug*)仅在开发环境下加载');
+        break;
+      default:
+        console.groupCollapsed(type);
+        console.log(data);
+    };
+    console.groupEnd();
+  }
 
-    function _init() {
-        _listenForLogMessages();
-        chrome.extension.sendMessage('isActive', function(response) {
-            if (response === false) {
-                return _stopListening();
-            }
-            return _initStorage();
-        });
-
-        chrome.extension.sendMessage('ready', function(queuedRequests) {
-            if (queuedRequests) {
-                queuedRequests.forEach(function(request) {
-                    _process(request);
-                });
-            }
-        });
+  function _handleHeaderUpdate(request, sender, sendResponse) {
+    if (request.name === "header_update") {
+      // if this is not a header update don't do anything
+      _process(request.details);
+      return sendResponse("done");
     }
-    _init();
+  }
+
+  function _listenForLogMessages() {
+    chrome.extension.onMessage.addListener(_handleHeaderUpdate);
+  }
+
+  function _stopListening() {
+    chrome.extension.onMessage.removeListener(_handleHeaderUpdate);
+  }
+
+  function _initStorage() {
+    chrome.extension.sendMessage("localStorage", function(response) {
+      local_storage = response;
+    });
+  }
+
+  function _init() {
+    _listenForLogMessages();
+    chrome.extension.sendMessage('isActive', function(response) {
+      if (response === false) {
+        return _stopListening();
+      }
+      return _initStorage();
+    });
+
+    chrome.extension.sendMessage('ready', function(queuedRequests) {
+      if (queuedRequests) {
+        queuedRequests.forEach(function(request) {
+          _process(request);
+        });
+      }
+    });
+  }
+  _init();
 })();
